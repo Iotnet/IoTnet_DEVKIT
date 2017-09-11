cf33173e4c62b2453ebee5810925a77c94cab849
iotnet <gperez@iotnet.mx>
2017-09-11 12:28:28 -0500

    Actualizacion

---
+++ Ejemplos/sensor_temperatura/sensor_temperatura.ino
@@ -0,0 +1,88 @@
+#define SigfoxDeb
+#include <SoftwareSerial.h>
+#ifdef SigfoxDeb
+SoftwareSerial mySerial(0, 1); // RX, TX
+#endif
+
+const int sensorPin = A0;
+const float baselinetime=20.0;
+
+char RespuestaSigfox[50];
+char ID[51];
+char PAC[51];
+
+void setup() 
+{
+  Serial.begin(9600);
+  #ifdef SigfoxDeb
+  mySerial.begin(9600);
+  #endif
+  //pinMode(4, OUTPUT);
+  pinMode(7, OUTPUT);
+  #ifdef SigfoxDeb
+  mySerial.println("\n\n\n\n\n\n\n\n\rInicio");
+  #endif
+}
+
+void leer_sensor()
+{
+  String bufer="AT$SF=";
+  char payload[20];
+  int sensorVal=analogRead(sensorPin);
+  Serial.print("sensor Value; ");
+  Serial.print(sensorVal);
+  float voltaje=(sensorVal/1024.0)*5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltaje);
+  Serial.print(", degrees C: ");
+  float temp=((voltaje-0.5)*100)+1;
+  Serial.println(temp);
+  //convierte el dato a bytes y lo agrega a nuestro mensaje a enviar
+  byte* a1 = (byte*) &temp;  
+  String str1,str2,str3,str4;
+  str1=  String(a1[0], HEX);  
+  str2=  String(a1[1], HEX);
+  str3=  String(a1[2], HEX);
+  str4=  String(a1[3], HEX);
+  bufer+=0+str1+str2+str3+str4;
+  bufer.toCharArray(payload,16);
+  //
+  digitalWrite(7, HIGH);
+  delay(1000);
+  enviarcomandoATSigfox("AT");
+  enviarcomandoATSigfox("AT$RC");
+  enviarcomandoATSigfox(payload);
+  digitalWrite(7, LOW);
+}
+
+void loop() 
+{
+  leer_sensor();
+  delay(300000);
+}
+
+void enviarcomandoATSigfox(char* comandoAT){
+  unsigned long x=0;
+  #ifdef SigfoxDeb
+  mySerial.print("\r\n\tSigfox-->");
+  mySerial.println(comandoAT);
+  #endif
+  while( Serial.available() > 0) Serial.read();
+  x = 0;
+  memset(RespuestaSigfox, '\0',sizeof(RespuestaSigfox)); 
+  Serial.print(comandoAT);
+  Serial.print("\r\n");
+  while(true){
+    if(Serial.available() != 0){   
+      RespuestaSigfox[x] = Serial.read();
+      x++;
+      if (strstr(RespuestaSigfox, "\n") != NULL){
+        #ifdef SigfoxDeb
+        mySerial.print("Comando OK\n\r");
+        mySerial.println(RespuestaSigfox);
+        #endif
+        break;
+      }
+    }
+  }
+}
