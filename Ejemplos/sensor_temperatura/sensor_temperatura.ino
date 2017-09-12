393ebb7f12c45af531a82b88d7158321c44666f0
iotnet <gperez@iotnet.mx>
2017-09-12 14:24:32 -0500

    Actualizacion

--- Ejemplos/sensor_temperatura/sensor_temperatura.ino
+++ Ejemplos/sensor_temperatura/sensor_temperatura.ino
@@ -31,7 +31,7 @@ void leer_sensor()
   int sensorVal=analogRead(sensorPin);
   float voltaje=(sensorVal/1024.0)*5.0;
   Serial.print("Grados Cº: ");
-  float temp=((voltaje-0.5)*100)+1;
+  float temp=((voltaje)*100)+1;
   Serial.println(temp);
   //convierte el dato a bytes y lo agrega a nuestro mensaje a enviar
   byte* a1 = (byte*) &temp;  
